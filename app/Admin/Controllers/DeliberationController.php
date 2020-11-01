5bb808c4f075d8237246eb0bc14ddf1e4fb8e4a1
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2020-11-01 02:53:52 +0100

    Page déliberation

--- app/Admin/Controllers/DeliberationController.php
+++ app/Admin/Controllers/DeliberationController.php
@@ -7,15 +7,22 @@ use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\AnneeAccademique;
 
 class DeliberationController extends Controller
 {
     public function index(Content $content)
     {
+        $list_annee_acads = DB::table('annee_accademiques')->get();
+
         return $content
             ->title('Délibération')
             ->description('Page de délibération')
             ->row(Dashboard::title())
-            ->view('admins.deliberation');
+            ->view('admins.deliberation', [
+                'list' => $list_annee_acads
+            ]);
     }
 }
--- resources/views/admins/deliberation.blade.php
+++ resources/views/admins/deliberation.blade.php
@@ -1,3 +1,17 @@
 <div class="container">
-    <h2>Délibération</h2>
+    @if (!empty($list))
+        <div class="row">
+            @foreach ($list as $el)
+                <div class="col-lg-3">
+                    <div class="card">
+                        <div class="card-,o;;;;;">   
+                            <h1>
+                                <a href="/deliberations/{{ $el->id }}">{{ $el->libelle_annee }}</a>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endif
 </div>
